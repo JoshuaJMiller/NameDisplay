79d3483899a834e06a7c00cec4a11c89933a574e
JoshuaJMiller <joshua.miller1014@gmail.com>
2021-10-18 15:48:21 -0700

    init commit

---
+++ TextManipLibrary/TextManipulator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextManipLibrary
+{
+    public class TextManipulator
+    {
+        public string reverseText(string p_input)
+        {
+            List<char> reversedList = new List<char>();
+            reversedList = p_input.ToList<char>();
+            reversedList.Reverse();
+            string reversed = new string (reversedList.ToArray());
+            return reversed;
+        }
+
+        public string shuffleText(string p_input)
+        {
+            char[] array = p_input.ToCharArray();
+            Random rng = new Random();
+
+            int n = array.Length;
+
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+
+            return new string(array);
+        }
+    }
+}
---
+++ WPFNameApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TextManipLibrary;
+
+namespace WPFNameApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public TextManipulator TM = new TextManipulator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void displayButton_Click(object sender, RoutedEventArgs e)
+        {
+            textDisplay.Text = $"{firstNameBox.Text} {lastNameBox.Text}";
+        }
+
+        private void reverseButton_Click(object sender, RoutedEventArgs e)
+        {
+            string reversed = TM.reverseText($"{firstNameBox.Text} {lastNameBox.Text}");
+            textDisplay.Text = reversed;
+        }
+        
+        private void shuffleButton_Click(object sender, RoutedEventArgs e)
+        {
+            string shuffled = TM.shuffleText($"{firstNameBox.Text} {lastNameBox.Text}");
+            textDisplay.Text = shuffled;
+        }
+    }
+}
